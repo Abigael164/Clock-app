c9874b9cc946dddc946daa705a8544bf9dd42bf1
Abigael Jeptoo <jeptooabigael164@gmail.com>
2025-11-11 11:38:57 -0800

    Renders clock component

---
+++ Clockimport.jsx
@@ -0,0 +1,11 @@
+import './Clock.jsx'
+import './Clockimport.css'
+
+const Clockimport = () => {
+    return(
+        <div className='container'>
+            <Clock />
+        </div>
+    )
+}
+export default Clockimport;
